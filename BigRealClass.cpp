4123b06e42e8f5c6cc51a284b42603716bae70e2
malakwalid32 <101887218+malakwalid32@users.noreply.github.com>
2022-11-14 22:57:37 +0200

    Update BigRealClass.cpp

--- BigRealClass.cpp
+++ BigRealClass.cpp
@@ -1,13 +1,16 @@
-
-#include "BigRealClass.h"
+#include "BigReal.h"
 #include "BigDecimalIntClass.h"
 #include "BigDecimalIntClass.cpp"
 
-// #include "BigDecimalIntClass.h"
+//#include "BigDecimalIntClass.h"
 
 BigReal::BigReal(string realNumber){
     string before,after;
     int z;
+    while (realNumber.at(realNumber.length() - 1)=='0')
+    {
+        realNumber.pop_back();
+    }
     for(int i = 0; i < realNumber.size(); i++){
         if(realNumber[i] == '.') i = z;
     }
@@ -35,6 +38,7 @@ BigReal::BigReal (BigReal&& other){
 BigReal& BigReal::operator= (BigReal& other){
     bPoint = other.bPoint;
     aPoint = other.aPoint;
+    return *this;
 }
 
 BigReal& BigReal::operator= (BigReal&& other){
@@ -42,4 +46,137 @@ BigReal& BigReal::operator= (BigReal&& other){
     aPoint = move(other.aPoint);
     return *this;
 }
+char BigReal :: getsign(){
+    return sign;
+}
+int BigReal:: getsize(){
+    return size;
+}
+bool BigReal:: operator>(BigReal other)
+{
+    if (sign=='+'&&other.sign=='-')
+        return true;
+    else if (sign=='-'&&other.sign=='+')
+        return false;
+    if ((bPoint.size()>other.bPoint.size())&&(sign=='+')&&(other.sign=='+'))
+        return true;
+    if ((bPoint.size()> other.bPoint.size())&&(sign=='-')&&(other.sign=='-'))
+        return false;
+    if ((bPoint.size()==other.bPoint.size())&&(sign=='+')&&(other.sign=='+'))
+    {
+        for (int i=0;i<bPoint.size();i++)
+        {
+           if(bPoint.getNumber()[i] >other.bPoint.getNumber()[i])
+              return true;
+            else if (bPoint.getNumber()[i]<other.bPoint.getNumber()[i])
+                return false;
+
+        }
+        for (int i=0;i<bPoint.size();i++)
+        {
+            if (bPoint.getNumber()[i]==other.bPoint.getNumber()[i])
+                {
+                    if (aPoint.size()>other.aPoint.size()){
+                        for (int i=0;i<aPoint.size();i++){
+                                other.aPoint.getNumber()+='0';
+                        }
+                        for (int i=0 ;i<aPoint.size();i++){
+                            if(aPoint.getNumber()[i]>other.aPoint.getNumber()[i])
+                                return true;
+                            else
+                                return false;
+                        }
+                    }
+                    else if (aPoint.size()<other.aPoint.size()){
+                        for (int i=0;i<other.aPoint.size();i++){
+                            aPoint.getNumber()+='0';
+                        }
+                        for (int i=0 ;i<other.aPoint.size();i++){
+                            if(aPoint.getNumber()[i]>other.aPoint.getNumber()[i])
+                                return true;
+                            else
+                                return false;
+
+                        }
+                    }
+                }
+            }
+        }
+}
+
+
+bool BigReal:: operator==(BigReal other)
+{
+    if (sign=='+'&&other.sign=='+')
+    {
+        if (bPoint.size()==other.bPoint.size())
+        {
+            if (aPoint.size()==other.aPoint.size())
+            {
+                for (int i=0;i<bPoint.size();i++)
+                    {
+                        if (bPoint.getNumber()[i]==other.bPoint.getNumber()[i])
+                            return true;
+                        else if (bPoint.getNumber()[i]!=other.bPoint.getNumber()[i])
+                            return false;
+                    }
+                for (int i=0;i<aPoint.size();i++)
+                {
+                    if (aPoint.getNumber()[i]==other.aPoint.getNumber()[i])
+                        return true;
+                    else if (aPoint.getNumber()[i]!=other.aPoint.getNumber()[i])
+                            return false;
+                }
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    else
+        return false;
+    if (sign=='-'&&other.sign=='-')
+    {
+        if (bPoint.size()==other.bPoint.size())
+        {
+            if (aPoint.size()==other.aPoint.size())
+            {
+                for (int i=0;i<bPoint.size();i++)
+                    {
+                        if (bPoint.getNumber()[i]==other.bPoint.getNumber()[i])
+                            return true;
+                        else if (bPoint.getNumber()[i]!=other.bPoint.getNumber()[i])
+                            return false;
+                    }
+                for (int i=0;i<aPoint.size();i++)
+                {
+                    if (aPoint.getNumber()[i]==other.aPoint.getNumber()[i])
+                        return true;
+                    else if (aPoint.getNumber()[i]!=other.aPoint.getNumber()[i])
+                            return false;
+                }
+            }
+
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    else
+        return false;
+}
+ostream &operator<<(ostream &out, BigReal number)
+{
+    string str = number.getNumber();
+    char s = number.getsign();
+    if (s == '-')
+        out << '-';
+    for (int i = 0; i < str.size(); i++)
+        out << str[i];
+    return out;
+}
+
+
 
